412752f903b6924745a358d03393e0715aeda7bc
Zyther <gregocm1000@gmail.com>
2024-11-25 21:09:07 -0300

    Added claims during user authentication.

--- SwiftConverter/Controllers/AuthenticationController.cs
+++ SwiftConverter/Controllers/AuthenticationController.cs
@@ -38,6 +38,8 @@ namespace Web.Controllers
                 var securityPassword = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(_config["Authentication:SecretForKey"]));
                 var signature = new SigningCredentials(securityPassword, SecurityAlgorithms.HmacSha256);
                 var claimsForToken = new List<Claim>();
+                claimsForToken.Add(new Claim("isAdmin", user.IsAdmin.ToString()));
+                claimsForToken.Add(new Claim("username", user.Username));
 
                 var jwtSecurityToken = new JwtSecurityToken(
                     _config["Authentication:Issuer"],
