aa07bac1b3b53fa3972aafd38ad00f2650cfd554
Zyther <gregocm1000@gmail.com>
2024-12-03 14:27:17 -0300

    Added some length check decorators.

--- Common/DTO/UserForCreation.cs
+++ Common/DTO/UserForCreation.cs
@@ -5,10 +5,13 @@ namespace Common.DTO
     public class UserForCreation
     {
         [Required]
+        [MaxLength(50)]
         public required string Username { get; set; }
         [Required]
+        [MaxLength(30)]
         public required string Password { get; set; }
         [Required]
+        [MaxLength(320)]
         public required string Email { get; set; }
     }
 }
